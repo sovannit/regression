37d5ec2f2386d006da230c755bee92244eee5095
tvongerlach <tassilo.v.gerlach@gmail.com>
2016-12-18 12:15:40 -0500

    Opt using backwards elimination

--- Multiple Linear Regression/multiple_linear_regression_model.py
+++ Multiple Linear Regression/multiple_linear_regression_model.py
@@ -52,6 +52,18 @@ X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
-X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
